e006c0a85a4860a8811cc37e3bcf2e568bc4530f
tony.vo <tung007@e.ntu.edu.sg>
2019-01-12 20:45:15 +0700

    World Population Data Map

--- data_analysis/americans.py
+++ data_analysis/americans.py
@@ -1,2 +1,12 @@
-import pygal
-wm = pygal.Worldmap()
+from pygal.maps.world import World
+#create an instance of world map class
+wm = World()
+#set title for the map
+wm.title = 'North, Central and South Americal'
+#set up new color and add new color to the left
+wm.add('North America', {'ca':34126000,'mx':113423000,'us':309349000})
+wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'pa', 'sv'])
+wm.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf',
+	'gy', 'pe', 'py', 'sr', 'uy', 've'])
+#render to a file
+wm.render_to_file('americas.svg')
--- data_analysis/world_population.py
+++ data_analysis/world_population.py
@@ -1,9 +1,12 @@
 import json
+from appscript import *
+from pygal.maps.world import World
 from countries import get_country_code
 filename = 'population_data.json'
 with open(filename) as f:
 	pop_data = json.load(f)
 #print 2010 population of each country
+cc_population = {}
 for data_row in pop_data:
 	if(data_row['Year'] == '2010'):
 		country_name = data_row['Country Name']
@@ -12,7 +15,15 @@ for data_row in pop_data:
 		population = int(float(data_row['Value']))
 		country_code = get_country_code(country_name)
 		if country_code:
+			cc_population[country_code] = population
 			print(country_code + ": "+str(population))
 		else:
 			print("ERROR - "+country_name)
 		#print(country_name + " : " + str(population))
+wm = World()
+wm.title = 'World Population 2020 by Country'
+wm.add('2010', cc_population)
+wm.render_to_file('"world_population.svg"')
+#open file
+safari = app("Safari")
+safari.make(new = k.document, with_properties={k.URL:"file:///Users/tungvo/Desktop/Portfolio/data_analysis/world_population.svg"})
